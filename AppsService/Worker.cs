7fedc45d7a7d87a84af26c10620c1dd9de7f9fa9
mohammadreza <mohammadreza.mirzakhani.1999@gmail.com>
2025-08-20 19:14:00 +0330

    add update

--- AppsService/Worker.cs
+++ AppsService/Worker.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using Microsoft.Data.Sqlite;
+
 namespace AppsService
 {
     public class Worker : BackgroundService
@@ -13,12 +16,34 @@ namespace AppsService
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                
-                
+                object Result;
+                int Numbers;
+                List<string> Directories = new List<string>();
 
+                SqliteConnection connection = new SqliteConnection("Data Source=Apps.db");
+                SqliteCommand command = new SqliteCommand();
 
+                connection.Open();
+                command.Connection = connection; 
+                command.CommandText = "SELECT MAX(AppID) AS AppID FROM Apps";
+                Result = command.ExecuteScalar();
+                connection.Close();
 
+                Numbers = Convert.ToInt32(Result);
 
+                for (int Check = 0; Check <= Numbers; Check++)
+                {
+                    connection.Open();
+                    command.CommandText = $"SELECT AppDirectory From Apps Where AppID = {Numbers}";
+                    Result = command.ExecuteScalar();
+                    connection.Close();
+                    Directories.Add(Result.ToString());
+                }
+
+                for(int Check = 0; Check <= Directories.Count -1; Check++)
+                {
+                    Process.Start(Directories[Check]);
+                }
 
                 await Task.Delay(Timeout.Infinite, stoppingToken);
             }
